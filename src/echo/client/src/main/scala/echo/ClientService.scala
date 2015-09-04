84463e9efe0e6801c6cbfa681083e460db6b6d96
Yoshi <yoshi@1o1.co.jp>
2015-09-04 16:54:08 +0900

    fix

--- src/echo/client/src/main/scala/echo/ClientService.scala
+++ src/echo/client/src/main/scala/echo/ClientService.scala
@@ -5,24 +5,27 @@ import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.{Future, ExecutionContext}
 
+object ClientServiceImpl {
+  def apply(e:Service) = new ClientServiceImpl {
+    override val serverEndpoint = e
+  }
+}
+
 trait ClientServiceImpl extends Service {
   // 
   def serverEndpoint : Service 
 
-  implicit def actorTimeout:Timeout
-
   def echo(message:String)(implicit ec:ExecutionContext) : Future[String] = {
-    (server ? Protocol.EchoRequest(message))
-      .mapTo[Protocol.EchoResponse]
-      .map(r => r.message)
+    serverEndpoint.echo(message)
   }
 }
 
 object ClientServiceActor {
-  def props(server:ActorRef)(implicit ec:ExecutionContext, to:Timeout) : Props = Props(new ClientServiceActor(new ActorService(server)))
+  def props(server:ActorRef)(implicit ec:ExecutionContext, to:Timeout) : Props = Props(new ClientServiceActor(ActorService(server)))
   def props(server:Service)(implicit ec:ExecutionContext, to:Timeout) : Props = Props(new ClientServiceActor(server))
 }
-class ClientServiceActor(override val serverEndpoint:Service)(implicit val ec:ExecutionContext, val actorTimeout:Timeout) extends Actor with ServiceActorLike with ClientServiceImpl {
+
+class ClientServiceActor(override val serverEndpoint:Service)(implicit val ec:ExecutionContext) extends Actor with ServiceActorLike with ClientServiceImpl {
   
 }
 
